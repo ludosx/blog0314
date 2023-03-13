c01fb229c89c53db99235757c4b1a0c7cf9bb7c5
Your Name <you@example.com>
2023-03-13 11:31:21 -0500

    with routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BlogController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BlogController::class, 'loginView'])->middleware('guest')->name('home');
+Route::post('/login', [BlogController::class, 'login'])->name('login');
+Route::get('/register', [BlogController::class, 'registerView'])->middleware('guest')->name('newregister');
+Route::post('/register', [BlogController::class, 'register'])->name('saveregister');
+Route::get('/dashboard', [BlogController::class, 'dashboardView'])->middleware('auth')->name('dashboard');
+Route::post('/logout', [BlogController::class, 'logout'])->name('logout');
+Route::get('/post', [BlogController::class, 'newPost'])->middleware('auth')->name('newpost');
+Route::post('/post', [BlogController::class, 'savePost'])->name('savepost');
+Route::get('/answerpost', [BlogController::class, 'newAnswer'])->middleware('auth')->name('newanswer');
+Route::post('/answerpost', [BlogController::class, 'saveAnswer'])->name('saveanswer');
